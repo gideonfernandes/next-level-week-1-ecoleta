902c9ae52f772a96750d735454bfac4a1667f2e1
Gideon Fernandes <gide10062014@gmail.com>
2020-06-07 21:48:29 -0300

    Add backend

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import routes from "./routes";
+import path from "path";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
+app.listen(3333, () => {
+	console.log("🚨️ Servidor iniciado!");
+});
